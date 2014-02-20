7d2d8b988ec4f4725ee47810e3591438b6670bb0
rgmoreno <garcia.moreno.ruben@gmail.com>
2014-02-20 01:26:17 +0000

    Creats correct meshes for shadows

---
+++ Assets/Scripts/SVGReader.cs
@@ -0,0 +1,244 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Xml;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SVGReader : MonoBehaviour
+{
+    public TextAsset svgFile;
+    public Material svgMaterial;
+
+    private float width;
+    private float height;
+
+    void Start()
+    {
+        read();
+    }
+
+    private void read()
+    {
+        XmlDocument document = new XmlDocument();
+        document.LoadXml(svgFile.text);
+
+        SVG svg = new SVG();
+
+        XmlNode svgNode = document.SelectSingleNode("svg");
+
+        width = float.Parse(svgNode.Attributes.GetNamedItem("width").Value);
+        height = float.Parse(svgNode.Attributes.GetNamedItem("height").Value);
+
+        XmlNodeList groupNodeList = svgNode.SelectNodes("g");
+        foreach (XmlNode groupNode in groupNodeList)
+        {
+            string display = groupNode.Attributes.GetNamedItem("style").Value.Split(':')[1];
+            if (display != null && display == "inline")
+            {
+                SVGGroup group = new SVGGroup();
+                
+                XmlNodeList pathNodeList = groupNode.SelectNodes("path");
+                foreach (XmlNode pathNode in pathNodeList)
+                {
+                    SVGPath svgPath = new SVGPath();
+
+                    string path = pathNode.Attributes.GetNamedItem("d").Value;
+                    string id = pathNode.Attributes.GetNamedItem("id").Value;
+                    string style = pathNode.Attributes.GetNamedItem("style").Value;
+
+                    svgPath.vertexList = parsePathVertexList(path);
+                    svgPath.color = parsePathColor(style);
+
+                    group.pathList.Add(svgPath);
+                }
+
+                svg.groupList.Add(group);
+            }
+        }
+
+        for (int i = 0; i < svg.groupList.Count; i++)
+        {
+            SVGGroup group = svg.groupList[i];
+            for (int j = 0; j < group.pathList.Count; j++)
+            {
+                SVGPath path = group.pathList[j];
+                
+                Mesh mesh = createMesh(path.vertexList, path.color);
+
+                string assetpath = "Assets/SVGMeshes";
+                AssetDatabase.CreateAsset(mesh, assetpath + "/" + svgFile.name + "_" + j + ".asset");
+                AssetDatabase.SaveAssets();
+
+                GameObject gameObject = new GameObject(svgFile.name + "_" + j);
+                
+                MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+                MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+                
+                meshFilter.mesh = mesh;
+                meshRenderer.sharedMaterial = svgMaterial;
+                meshRenderer.castShadows = false;
+                meshRenderer.receiveShadows = false;
+
+                //Create Hole Cup Prefab
+                string svgpath = "Assets/SVG";
+                if (!Directory.Exists(svgpath)) Directory.CreateDirectory(svgpath);
+                PrefabUtility.CreatePrefab(svgpath + "/" + svgFile.name + ".prefab", gameObject);
+            }
+        }
+    }
+
+    private List<Vector2> parsePathVertexList(string path)
+    {
+        //Path Data: M = moveto - L = lineto - H = horizontal lineto - V = vertical lineto - C = curveto - S = smooth curveto
+        //Q = quadratic Bézier curve - T = smooth quadratic Bézier curveto - A = elliptical Arc - Z = closepath
+
+        int index = 0;
+        List<Vector2> vertexList = new List<Vector2>();
+        Vector2 lastVector = new Vector2(0, height);
+        bool isAbsolute = false;
+        
+        string[] vertices = path.Split(' ');
+        for (int i = 0; i < vertices.Length; i++)
+        {           
+            string value = vertices[i];
+            if (value == "m" || value == "l")
+            {
+                isAbsolute = false;
+            }
+            else if (value == "M" || value == "L" )
+            {
+                isAbsolute = true;
+            }
+            else if (value != "z" && value != "Z")
+            {
+                string[] vertex = value.Split(',');
+
+                float x;
+                float y;
+
+                if (isAbsolute)
+                {
+                    x = float.Parse(vertex[0]);
+                    y = -(float.Parse(vertex[1]) - height);
+                }
+                else
+                {
+                    x = lastVector.x + float.Parse(vertex[0]);
+                    y = lastVector.y - float.Parse(vertex[1]);
+                }
+
+                lastVector = new Vector2(x, y);
+                vertexList.Add(lastVector);
+
+                GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                quad.name = index.ToString();
+                quad.transform.localScale = Vector3.one * 2.5f;
+                quad.transform.position = new Vector3(lastVector.x, lastVector.y, 0);
+
+                index++;
+            }
+        }
+
+        return vertexList;
+    }
+
+    private Color parsePathColor(string style)
+    {
+        string hexcolor = style.Split(';')[0].Split(':')[1];
+        Color color = HexToColor(hexcolor);
+
+        return color;
+    }
+
+    private Mesh createMesh(List<Vector2> vertexList, Color color)
+    {
+        Vector3[] vertices = new Vector3[vertexList.Count];
+        Vector2[] uvs = new Vector2[vertexList.Count];
+        Color[] colors = new Color[vertexList.Count];
+
+        Triangulator triangulator = new Triangulator(width, height, vertexList);
+        int[] triangles = triangulator.Triangulate();
+
+        for (int i = 0; i < vertexList.Count; i++)
+        {
+            vertices[i] = new Vector3(vertexList[i].x, vertexList[i].y, 0);
+            uvs[i] = new Vector2(vertexList[i].x, vertexList[i].y);
+            colors[i] = color;
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.uv = uvs;
+        mesh.colors = colors;
+
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+        mesh.Optimize();
+
+        return mesh;
+    }
+
+    public static string ColorToHex(Color color)
+    {
+        string hex = color.r.ToString("X2") + color.g.ToString("X2") + color.b.ToString("X2");
+        return hex;
+    }
+
+    public static Color HexToColor(string hex)
+    {
+        byte r = byte.Parse(hex.Substring(1, 2), System.Globalization.NumberStyles.HexNumber);
+        byte g = byte.Parse(hex.Substring(3, 2), System.Globalization.NumberStyles.HexNumber);
+        byte b = byte.Parse(hex.Substring(5, 2), System.Globalization.NumberStyles.HexNumber);
+
+        return new Color32(r, g, b, 255);
+    }
+}
+
+public class SVG
+{
+    public List<SVGGroup> groupList;
+
+    public SVG()
+    {
+        groupList = new List<SVGGroup>();
+    }
+}
+
+public class SVGGroup
+{
+    public List<SVGPath> pathList;
+
+    public SVGGroup()
+    {
+        pathList = new List<SVGPath>();
+    }
+}
+
+public class SVGPath
+{
+    public List<Vector2> vertexList;
+    public Color color;
+
+    public SVGPath()
+    {
+        vertexList = new List<Vector2>();
+    }
+}
+
+public class Triangle
+{
+    public int v1Index;
+    public int v2Index;
+    public int v3Index;
+}
+
+public class Vertex
+{
+    public float x;
+    public float y;
+    public Color color;
+}
---
+++ Assets/Scripts/Triangulator.cs
@@ -0,0 +1,298 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Triangulator
+{
+    private List<Vector2> vertexList;
+    private List<int> triangleList;
+
+    private List<int> vertexIndexList;
+    private List<int> earVertexList;
+    private List<int> convexVertexList;
+    private List<int> reflexVertexList;
+
+    private bool isClockWise;
+
+    public Triangulator(float width, float height, List<Vector2> vertices)
+    {
+        vertexList = vertices;
+
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            Vector2 vertex = new Vector2(vertexList[i].x - width / 2, vertexList[i].y - height / 2);
+            vertexList[i] = vertex;
+        }
+
+        vertexIndexList = new List<int>();
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            Vector2 vertex = new Vector2();
+            vertexIndexList.Add(i);
+        }
+
+        isClockWise = isPolygonClockWise();
+        Debug.Log("isClockWise: " + isClockWise);
+    }
+
+    public int[] Triangulate()
+    {
+        if (vertexList.Count < 3)
+        {
+            Debug.LogError("Cannot triangulate with less than 3 vertices");
+        }
+
+        // Set up phase
+        initializeConvexList();
+        initializeReflexList();
+        initializeEarList();
+
+        // Run the algorithm
+        return InnerTriangulate();
+    }
+
+    private void initializeConvexList()
+    {
+        convexVertexList = new List<int>();
+        for (int i = 0; i < vertexList.Count; i++)
+        {
+            if (isConvex(i)) convexVertexList.Add(i);
+        }
+    }
+
+    private void initializeReflexList()
+    {
+        reflexVertexList = new List<int>();
+        for (int i = 0; i < vertexList.Count; i++)
+        {
+            if (isReflex(i)) reflexVertexList.Add(i);
+        }
+    }
+
+    private void initializeEarList()
+    {
+        earVertexList = new List<int>();
+        for (int i = 0; i < vertexList.Count; i++)
+        {
+            if (isEar(i)) earVertexList.Add(i);
+        }
+    }
+
+    private bool isConvex(int vertexIndex)
+    {
+        return !isReflex(vertexIndex);
+    }
+
+    private bool isReflex(int vertexIndex)
+    {
+        Vector2 v0 = vertexList[calculatePrevIndex(vertexIndex)];
+        Vector2 v1 = vertexList[vertexIndex];
+        Vector2 v2 = vertexList[calculateNextIndex(vertexIndex)];
+
+        Vector2 v1v0 = v1 - v0;
+        Vector2 v1v2 = v1 - v2;
+
+        //if (isClockWise)
+        //{
+        //    return angleSign(v1v0, v1v2) > 0;
+        //}
+        //else
+        //{
+            return angleSign(v1v0, v1v2) < 0;
+        //}
+    }
+
+    private float angleSign(Vector2 v1, Vector2 v2)
+    {
+        // the vector that we want to measure an angle from
+        Vector3 referenceForward = new Vector3(v1.x, 0, v1.y);
+ 
+        // the vector perpendicular to referenceForward (90 degrees clockwise)
+        // (used to determine if angle is positive or negative)
+        Vector3 referenceRight = Vector3.Cross(Vector3.up, referenceForward);
+ 
+        // the vector of interest
+        Vector3 newDirection = new Vector3(v2.x, 0, v2.y);
+ 
+        // Get the angle in degrees between 0 and 180
+        float angle = Vector3.Angle(newDirection, referenceForward);
+ 
+        // Determine if the degree value should be negative.  Here, a positive value
+        // from the dot product means that our vector is on the right of the reference vector   
+        // whereas a negative value means we're on the left.
+        float sign = Mathf.Sign(Vector3.Dot(newDirection, referenceRight));
+ 
+        float finalAngle = sign * angle;
+        return finalAngle;
+    }
+
+    private bool isPolygonClockWise()
+    {
+        int concaveSum = 0;
+        int convexSum = 0;
+
+        for (int i = 0; i < vertexList.Count - 2; i++)
+        {
+            int i1 = (i + 1) > (vertexList.Count - 1) ? (i + 1) % vertexList.Count : (i + 1);
+            int i2 = (i + 2) > (vertexList.Count - 1) ? (i + 2) % vertexList.Count : (i + 2);
+
+            Vector3 p1 = new Vector3(vertexList[i].x, vertexList[0].y, 0);
+            Vector3 p2 = new Vector3(vertexList[i1].x, vertexList[1].y, 0);
+            Vector3 p3 = new Vector3(vertexList[i2].x, vertexList[2].y, 0);
+
+            Vector3 v1 = p1 - p2;
+            Vector3 v2 = p3 - p2;
+
+            if (Vector3.Cross(v1, v2).z > 0) convexSum++;
+            else concaveSum++;
+        }
+        
+        return convexSum > concaveSum;
+    }
+
+    private bool isEar(int vertexIndex)
+    {
+        Vector2 v0 = vertexList[calculatePrevIndex(vertexIndex)];
+        Vector2 v1 = vertexList[vertexIndex];
+        Vector2 v2 = vertexList[calculateNextIndex(vertexIndex)];
+
+        foreach (Vector2 vertex in vertexList)
+        {
+            if (vertex != v0 && vertex != v1 && vertex != v2)
+            {
+                if (isPointInTriangle(vertex, v0, v1, v2))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return isReflex(vertexIndex);
+    }
+
+    private bool isPointOnSameSide(Vector2 point1, Vector2 point2, Vector2 vertex1, Vector2 vertex2)
+    {
+        Vector3 v2v1 = new Vector3(vertex2.x, 0, vertex2.y) - new Vector3(vertex1.x, 0, vertex1.y);
+        Vector3 p1v1 = new Vector3(point1.x, 0, point1.y) - new Vector3(vertex1.x, 0, vertex1.y);
+        Vector3 p2v1 = new Vector3(point2.x, 0, point2.y) - new Vector3(vertex1.x, 0, vertex1.y);
+
+        Vector3 cp1 = Vector3.Cross(v2v1, p1v1);
+        Vector3 cp2 = Vector3.Cross(v2v1, p2v1);
+
+        return Vector3.Dot(cp1, cp2) >= 0;
+    }
+
+    private bool isPointInTriangle(Vector2 point, Vector2 tv1, Vector2 tv2, Vector2 tv3)
+    {
+        return isPointOnSameSide(point, tv1, tv2, tv3) && isPointOnSameSide(point, tv2, tv3, tv1) && isPointOnSameSide(point, tv3, tv1, tv2);
+    }
+
+    /// <summary>
+    /// If the vertex was reflex, see if it has become convex
+    /// If it is now convex, test for an ear.
+    /// If the vertex has become flat (collinear), then it
+    /// should be removed. Return true in this case.
+    /// If the vertex was convex, it will stay convex, but
+    /// we should test to see if it has become an ear.
+    /// </summary>
+    /// <param name="vertex">The vertex to adjust</param>
+    /// <returns>Nothing</returns>
+    private void FixUpVertex(int vertexIndex)
+    {
+        if (!convexVertexList.Contains(vertexIndex) && isConvex(vertexIndex))
+        {
+            reflexVertexList.Remove(vertexIndex);
+            convexVertexList.Add(vertexIndex);
+        }
+
+        if (!earVertexList.Contains(vertexIndex) && isEar(vertexIndex))
+        {
+            reflexVertexList.Remove(vertexIndex);
+            earVertexList.Add(vertexIndex);
+        }
+
+        
+        if (earVertexList.Contains(vertexIndex) && !isEar(vertexIndex))
+        {
+            if (isConvex(vertexIndex)) convexVertexList.Add(vertexIndex);
+            else if (isReflex(vertexIndex)) reflexVertexList.Add(vertexIndex);
+
+            earVertexList.Remove(vertexIndex);
+        }
+
+        return;
+    }
+
+    private int[] InnerTriangulate()
+    {
+        triangleList = new List<int>();
+
+        // Keep clipping ears until only 3 vertices (single triangle) remain
+        while (vertexIndexList.Count > 3)
+        {
+            if (earVertexList.Count == 0) break;
+
+            // Take the first ear in the list
+            int earIndex = earVertexList[0];
+
+            int prevIndex = calculatePrevIndex(earIndex);
+            int nextIndex = calculateNextIndex(earIndex);
+
+            // Add the ear to our output triangle list
+            triangleList.Add(prevIndex);
+            triangleList.Add(earIndex);
+            triangleList.Add(nextIndex);
+
+            // Remove the ear from the ear list and the vertex from the vertex list
+            earVertexList.RemoveAt(0);
+            vertexIndexList.Remove(earIndex);
+
+            if (vertexList.Count == 3)
+            {
+                // Normal early exit
+                break;
+            }
+
+            // Examine the two remaining vertices.
+            FixUpVertex(prevIndex);
+            FixUpVertex(nextIndex);
+        }
+
+        // Add the remaining triangle
+        triangleList.Add(vertexIndexList[0]);
+        triangleList.Add(vertexIndexList[1]);
+        triangleList.Add(vertexIndexList[2]);
+
+        return triangleList.ToArray();
+    }
+
+    private int calculatePrevIndex(int vertexIndex)
+    {
+        int index = vertexIndexList.IndexOf(vertexIndex);
+        int prevIndex = index == 0 ? vertexIndexList.Count - 1 : index - 1;
+        return vertexIndexList[prevIndex];
+    }
+
+    private int calculateNextIndex(int vertexIndex)
+    {
+        int index = vertexIndexList.IndexOf(vertexIndex);
+        int nextIndex = index == vertexIndexList.Count - 1 ? 0 : index + 1;
+        return vertexIndexList[nextIndex];
+    }
+
+    private void printVertexInfo()
+    {
+        string ears = "";
+        foreach (int index in earVertexList)
+        {
+            ears += index + " - ";
+        }
+        Debug.Log("EARS: " + ears);
+        string convex = "";
+        foreach (int index in convexVertexList)
+        {
+            convex += index + " - ";
+        }
+        Debug.Log("CONVEX: " + convex);
+    }
+}
